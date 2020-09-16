adc862fb0f55dc714f450e6eaf106cec0f9a1f76
Konstantin <mkonstn@gmail.com>
2020-09-16 22:19:55 +0300

    personal movie DB

--- js/script.js
+++ js/script.js
@@ -1,56 +1,22 @@
 "use strict";
 
-let num = 10.1;
 
-console.log(num);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","0");
 
-let name = 'this is sting';
-
-console.log(name);
-
-let undef;
-
-console.log(undef);
-
-let nl = null;
-
-console.log(nl);
-
-let arr = [1,2,'string',null];
-
-console.log(arr);
-console.log(arr[0]);
-
-let obj = {
-    name: "Ksant",
-    age: '42',
-    mf: 'male'
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj);
+const lastMovie = prompt("Один из последних просмотренных фильмов?","");
+const rate = +prompt("Его рейтинг?","1-10");
+const lastMovie2 = prompt("Один из последних просмотренных фильмов?","");
+const rate2 = +prompt("Его рейтинг?","1-10");
 
-//// alert, confirm, prompt, document.write
-// alert("ups");
-// let result = confirm("А вы уверены в ups?");
-// console.log(result);
-// let result = prompt("Напишите что нибудь пожалуйтса","сюда");
-// console.log(result);
-// document.write(result);
+personalMovieDB.movies[lastMovie] = rate;
+personalMovieDB.movies[lastMovie2] = rate2;
 
-//// Интерполяция.
-//alert(`Польователь. Имя: ${obj.name}, Возраст ${obj.age}, Пол ${obj.mf}. ` );
-
-//// Операторы
-let incr = 10,
-    decr = 10;
-
-// incr++;
-// decr--;
-
-console.log(++incr);
-console.log(--decr);
-// Остаток от деления.
-console.log(10%4);
-// Сравнение по типу данных
-console.log(2*4 === 8);
-console.log(2*4 === '8');
+console.log(personalMovieDB);
