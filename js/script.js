11b9f3496d53fdfb737bd675b9f8850eac6c27ae
Konstantin <mkonstn@gmail.com>
2020-09-20 00:18:03 +0300

    task 2 movie db

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 "use strict";
 
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","0");
+let numberOfFilms = 0;
+while (numberOfFilms == 0) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","0");
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,14 +14,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastMovie = prompt("Один из последних просмотренных фильмов?","");
-const rate = +prompt("Его рейтинг?","1-10");
-const lastMovie2 = prompt("Один из последних просмотренных фильмов?","");
-const rate2 = +prompt("Его рейтинг?","1-10");
 
-personalMovieDB.movies[lastMovie] = rate;
-personalMovieDB.movies[lastMovie2] = rate2;
+for (let i = 1; i <=2 ; i++) {
+    let lastMovie = "";
+    let rate = 0;
+    while (lastMovie == null || lastMovie.length == 0) {
+        lastMovie = prompt("Один из последних просмотренных фильмов?","");
+    }
+    while (rate == null || rate == 0 ) {
+        rate = +prompt("Его рейтинг?","1-10");
+    }    
+    personalMovieDB.movies[lastMovie] = rate;
+}
 
+if (personalMovieDB.count < 10 ){
+    console.log("Посмотрено мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошлка ошибка");
+};
 console.log(personalMovieDB);
 
 
