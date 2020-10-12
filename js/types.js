7ee556e93e87a0aa2b42b513eb8381127bacf037
Konstantin <mkonstn@gmail.com>
2020-10-13 00:33:28 +0300

    Работа с датами

--- js/types.js
+++ js/types.js
@@ -33,7 +33,7 @@ console.log(typeof(+"4"));
 
 
 
-//// 3.1 to boolean
+//// ----------- 3.1 to boolean  -----------
 // false - 0, '', null, undefined. NaN Все остальное - true
 
 let switcher = null;
@@ -54,3 +54,26 @@ console.log(typeof(Boolean("4")));
 //// 3.3 to !!
 console.log(typeof(!!"4444"));
 // boolean
+
+
+//// ----------- Даты -------------------------------------
+const now = new Date();
+console.log(now);
+const d1 = new Date('2005-05-01');
+//другой вариант - new Date.parse('2005-05-01');
+console.log(d1);
+//// При формировании даты на основе чисел, учитываются часовые пояса
+const d2 = new Date(2020,5,2,20);
+console.log(d2);
+//// Милисекунды с начала 1970 года. Чтобы получить даты до 70 года записывем отрицательные цифры.
+const d3 = new Date(0);
+console.log(d3);
+//1970-01-01T00:00:00.000Z
+
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay()); // Получить день недели (начиная с воскресенья)
+console.log(now.getHours());
+now.setHours(10);
+console.log(now.getHours());
