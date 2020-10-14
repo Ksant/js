de0db4118a4d547d4a76a1c26c4f00d4ac9fb8b4
Konstantin <mkonstn@gmail.com>
2020-10-14 22:47:39 +0300

    Определение размеров элементов.

---
+++ js/size.js
@@ -0,0 +1,14 @@
+// Размер области без учета padding и прокрутки 
+// const height = box.clientHeight;
+
+// Размер области с учетом  учетом padding и прокрукти.
+// const height = box.offsetHeght;
+
+// Выстота с учетом той, что скрыта за область прокрутки.
+// const height = box.scrollHeight;
+
+// Получить положение элемента
+// box.getBoundingClientRect(); 
+
+// Получение Computed (идут из CSS. Не путать с inline стилями) стилей. Только для чтения.
+// const style = windows.getComputedStyle(box);
