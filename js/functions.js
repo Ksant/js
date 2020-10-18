3362d7d52d9aa7a4c4312c8e78cb58fec634c659
Konstantin <mkonstn@gmail.com>
2020-10-18 14:35:58 +0300

    Функции конструкторы

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,23 @@ console.log(calc(1,3));
 // Стрелочная функция
 let calc2 = (a,b) => a+b;
 console.log(calc2(1,4));
+
+// --- Функции конструкторы
+const User = function(name, id){
+    this.name = name,
+    this.id = id,
+    this.human = true;
+    this.exit = function (){
+        console.log(`user ${this.name} is exit`);
+    };
+};
+
+User.prototype.hello = function() {
+    console.log(`Hello ${this.name}`);
+};
+
+const Freid = new User('Fried',1);
+const Dan = new User('Dan', 2);
+
+Freid.hello();
+Dan.exit();
